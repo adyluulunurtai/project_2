a0ce3ebfaf831d871d5b8af448d5bc4d0e8cc3a8
Nurtai <adyluulunurtai7@gmail.com>
2022-01-31 12:51:05 +0300

    vynesli v class display block i show i dobavili animation

--- js/script.js
+++ js/script.js
@@ -5,34 +5,42 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
     function contentHidden (){
         content.forEach(item =>{
-            item.style.display = "none";
+            item.classList.remove("show");
+            item.classList.add("hidden");
+            item.classList.remove("fade");
+           
         });
         tabs.forEach(item =>{
             item.classList.remove("tabheader__item_active");
-        })
+        });
 
-    };
+    }
+    contentHidden()
 
     function contentShow (i){
-        content[i].style.display="block";
+        content[i].classList.remove("hidden");
+        content[i].classList.add("show", "fade");
         tabs[i].classList.add("tabheader__item_active");
     }
+    contentShow(0);
 
     wrapperTabs.addEventListener('click', (event)=>{
       
         if(event.target && event.target.classList.contains("tabheader__item")){
-            tabs.forEach((item,i)=>{
+           tabs.forEach((item,i)=>{
                 if(event.target == item){
-                    contentHidden();
-                    contentShow(i);
+                   contentHidden();
+                   contentShow(i);
                 }
-            })
+             
+            });
         }
+      
 
-    })
-    
+    });
+
 
  
  
 
-})
+});
